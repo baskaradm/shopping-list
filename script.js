6d1e4fc1072846c4b506e37dd618c419f1c72f1f
baskaradm <marko.baskarad34@gmail.com>
2023-06-16 12:16:13 +0200

    Add to clearn btn and filter items not to show when have no items

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearButton = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 function addItem(e) {
   e.preventDefault();
@@ -19,7 +20,11 @@ function addItem(e) {
 
   const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
+
+  //Add li to the DOM
   itemList.appendChild(li);
+
+  checkUI();
 
   itemInput.value = '';
 }
@@ -40,7 +45,10 @@ function createIcon(classes) {
 
 function removeItem(e) {
   if (e.target.parentElement.classList.contains('remove-item')) {
-    e.target.parentElement.parentElement.remove();
+    if (window.confirm('Are your sure')) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
   }
 }
 
@@ -48,8 +56,23 @@ function clearItems() {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
+  checkUI();
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll('li');
+  console.log(items);
+  if (items.length === 0) {
+    clearButton.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearButton.style.display = 'block';
+    itemFilter.style.display = 'block';
+  }
 }
 
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearButton.addEventListener('click', clearItems);
+
+checkUI();
